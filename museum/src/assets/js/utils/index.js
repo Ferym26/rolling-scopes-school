9fb63550feea43f832036547362f01f987f2d0a1
ferym26 <ferym26@gmail.com>
2021-09-20 12:24:19 +0300

    feat: add header

--- museum/src/assets/js/utils/index.js
+++ museum/src/assets/js/utils/index.js
@@ -3,7 +3,6 @@ import svgPolifill from './_svgPolifill';
 import userAgentCheck from './_userAgentCheck';
 import lazyLoad from './_lazy';
 import validation from './_validation';
-// import scrollTop from '../../../mixins/scroll-top/script';
 
 const utils = () => {
 	global();
@@ -11,7 +10,6 @@ const utils = () => {
 	userAgentCheck();
 	lazyLoad();
 	validation();
-	scrollTop();
 	if (window.matchMedia(`(max-width: ${settings.mobBreakpoint - 1}px)`).matches) {
 		//
 	}
